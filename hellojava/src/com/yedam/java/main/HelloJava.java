63bc3727e4915f2178c2004357b2829d94c76bfc
Ujoonnee <69345466+Ujoonnee@users.noreply.github.com>
2021-12-28 09:50:51 +0900

    Update HelloJava.java

--- hellojava/src/com/yedam/java/main/HelloJava.java
+++ hellojava/src/com/yedam/java/main/HelloJava.java
@@ -6,7 +6,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to JAVA");
-		System.out.println();
+
 	}
 	
 }
