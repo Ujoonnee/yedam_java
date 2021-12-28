310ce49d371279b439400399d7c43a7845fd7b49
admin <admin@YD5-20>
2021-12-28 09:05:16 +0900

    First commit

---
+++ hellojava/src/com/yedam/java/main/HelloJava.java
@@ -0,0 +1,11 @@
+package com.yedam.java.main;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to JAVA");
+		System.out.println();
+	}
+	
+}
